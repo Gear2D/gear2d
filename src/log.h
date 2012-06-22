869d555b5818f6529583e5e683dfc1bde5d82dde
Leonardo Guilherme de Freitas <lgfreitas@gear2d.com>
2012-06-22 01:21:07 -0300

    Changes to the log interface

--- src/log.h
+++ src/log.h
@@ -21,31 +21,47 @@ namespace gear2d {
    */
   class log {
     public:
-      log(const std::string & trace, int verbosity = 0);
+      enum loglevel {
+        none = -1,
+        info,
+        warn,
+        err
+      };
+        
+      log(const std::string & trace = std::string(), loglevel verbosity = info);
       ~log();
       
       template <typename T>
-      inline log & operator()(const T&, int verbosity = -1);
+      inline log & operator()(const T&, loglevel verbosity = info);
       template <typename T1, typename T2>
-      inline log & operator()(const T1&, const T2&, int verbosity = -1);
+      inline log & operator()(const T1&, const T2&, loglevel verbosity = info);
       template <typename T1, typename T2, typename T3>
-      inline log & operator()(const T1&, const T2&, const T3&, int verbosity = -1);
+      inline log & operator()(const T1&, const T2&, const T3&, loglevel verbosity = info);
       template <typename T1, typename T2, typename T3, typename T4>
-      inline log & operator()(const T1&, const T2&, const T3&, const T4&, int verbosity = -1);
+      inline log & operator()(const T1&, const T2&, const T3&, const T4&, loglevel verbosity = info);
       template <typename T1, typename T2, typename T3, typename T4, typename T5>
-      inline log & operator()(const T1&, const T2&, const T3&, const T4&, const T5&, int verbosity = -1);
+      inline log & operator()(const T1&, const T2&, const T3&, const T4&, const T5&, loglevel verbosity = info);
       template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
-      inline log & operator()(const T1&, const T2&, const T3&, const T4&, const T5&, const T6&, int verbosity = -1);
+      inline log & operator()(const T1&, const T2&, const T3&, const T4&, const T5&, const T6&, loglevel verbosity = info);
       
       static void output(const std::string & filename, bool append=false);
       static void output(std::ostream &);
       
+      
     public:
-      static int verbosity;
+      static loglevel level;
       
     private:
       static int indent;
       static std::ostream logstream;
+      
+    private:
+      std::string trace;
+      loglevel verbosity;
+      
   };
 }
+
+#define tracelog \
+  gear2d::log trace(__PRETTY_FUNCTION__, log::info);
 #endif
