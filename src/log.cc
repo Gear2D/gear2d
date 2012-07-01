5eb99445f463cea9def12a0dd60050f9263b1e5c
Leonardo Guilherme de Freitas <lgfreitas@gear2d.com>
2012-06-30 21:27:32 -0300

    Removing output() from log class. Adding minimum and maximum loglevels.

--- src/log.cc
+++ src/log.cc
@@ -1,63 +1,30 @@
 #include "log.h"
 
+#include <iostream>
+#include <string>
+
 using namespace gear2d;
 
-const char * logstring[] = { "information: ", "warning: ", "error: " };
+const char * log::logstring[] = { "", "[E] ", "[W] ", "[I] ", "[M] ", "" };
 
-log::log(const std::string & trace, log::loglevel verbosity) 
+log::log(const std::string & trace, log::verbosity level) 
   : trace(trace)
-  , verbosity(verbosity) {
+  , level(level) {
   
-  if (verbosity < level) return;
-    logstream << "Entering " << trace << std::endl;
+  if (globalverb < level) return;
+  for (int i = 0; i < indent; i++) logstream << "  ";
+  logstream << "Entering " << trace << std::endl;
+  indent++;
 }
 
 log::~log() {
-  if (verbosity < level) return;
+  if (globalverb < level) return;
+  indent--;
+  for (int i = 0; i < indent; i++) logstream << "  ";
   logstream << "Leaving " << trace << std::endl;
 }
-
-template<typename T>
-log & log::operator() (const T & t, log::loglevel verbosity) {
-  logstream << logstring[verbosity] << t << std::endl;
-  
-}
-
-template <typename T1, typename T2>
-log & log::operator() (const T1 & t1, const T2 & t2, log::loglevel verbosity) {
-  logstream << logstring[verbosity] << t1 << " " << t2 << std::endl;
-}
-
-template <typename T1, typename T2, typename T3>
-log & log::operator() (const T1 & t1, const T2 & t2, const T3 & t3, log::loglevel verbosity) {
-  logstream << logstring[verbosity] << t1 << " " << t2 << " " << t3 << std::endl;
-}
-
-template <typename T1, typename T2, typename T3, typename T4>
-log & log::operator() (const T1 & t1, const T2 & t2, const T3 & t3, const T4 & t4, log::loglevel verbosity) {
-  logstream << logstring[verbosity] << t1 << " " << t2 << " " << t3 << " " << t4 << std::endl;
-}
-
-template <typename T1, typename T2, typename T3, typename T4, typename T5>
-log & log::operator() (const T1 & t1, const T2 & t2, const T3 & t3, const T4 & t4, const T5 & t5, log::loglevel verbosity) {
-  logstream << logstring[verbosity] << t1 << " " << t2 << " " << t3 <<  " " << t4 << " " << t5 << std::endl;
-}
-
-template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
-log & log::operator() (const T1 & t1, const T2 & t2, const T3 & t3, const T4 & t4, const T5 & t5, const T6 & t6, log::loglevel verbosity) {
-  logstream << logstring[verbosity] << t1 << " " << t2 << " " << t3 <<  " " << t4 << " " << t5 << " " << t6 << std::endl;
-}
-
-void log::output (const std::string & filename, bool append) {
-  
-}
-
-void log::output (std::ostream &) {
-
-}
-
 
 
 std::ostream log::logstream(std::cout.rdbuf());
 int log::indent = 0;
-log::loglevel log::level = log::info;
+log::verbosity log::globalverb = log::error;
--- src/log.h
+++ src/log.h
@@ -21,47 +21,127 @@ namespace gear2d {
    */
   class log {
     public:
-      enum loglevel {
-        none = -1,
+      enum verbosity {
+        minimum = 0,
+        error,
+        warning,
         info,
-        warn,
-        err
+        verbose,
+        maximum
       };
-        
-      log(const std::string & trace = std::string(), loglevel verbosity = info);
+      
+      log(const std::string & trace = std::string(), verbosity level = verbose);
       ~log();
       
       template <typename T>
-      inline log & operator()(const T&, loglevel verbosity = info);
+      inline log & operator()(const T&, verbosity level = verbose);
       template <typename T1, typename T2>
-      inline log & operator()(const T1&, const T2&, loglevel verbosity = info);
+      inline log & operator()(const T1&, const T2&, verbosity level = verbose);
       template <typename T1, typename T2, typename T3>
-      inline log & operator()(const T1&, const T2&, const T3&, loglevel verbosity = info);
+      inline log & operator()(const T1&, const T2&, const T3&, verbosity level = verbose);
       template <typename T1, typename T2, typename T3, typename T4>
-      inline log & operator()(const T1&, const T2&, const T3&, const T4&, loglevel verbosity = info);
+      inline log & operator()(const T1&, const T2&, const T3&, const T4&, verbosity level = verbose);
       template <typename T1, typename T2, typename T3, typename T4, typename T5>
-      inline log & operator()(const T1&, const T2&, const T3&, const T4&, const T5&, loglevel verbosity = info);
+      inline log & operator()(const T1&, const T2&, const T3&, const T4&, const T5&, verbosity level = verbose);
       template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
-      inline log & operator()(const T1&, const T2&, const T3&, const T4&, const T5&, const T6&, loglevel verbosity = info);
-      
-      static void output(const std::string & filename, bool append=false);
-      static void output(std::ostream &);
-      
+      inline log & operator()(const T1&, const T2&, const T3&, const T4&, const T5&, const T6&, verbosity level = verbose);
       
     public:
-      static loglevel level;
+      static verbosity globalverb;
       
     private:
       static int indent;
       static std::ostream logstream;
+      static const char * logstring[];
       
     private:
       std::string trace;
-      loglevel verbosity;
+      verbosity level;
       
   };
+  
+  template<typename T>
+  log & log::operator() (const T & t, log::verbosity level) {
+    if (globalverb < level) return *this;
+    for (int i = 0; i < indent; i++) logstream << "  ";
+    logstream << logstring[level] << t << std::endl;
+    return *this;
+  }
+  
+  template <typename T1, typename T2>
+  log & log::operator() (const T1 & t1, const T2 & t2, log::verbosity level) {
+    if (globalverb < level) return *this;
+    for (int i = 0; i < indent; i++) logstream << "  ";
+    logstream << logstring[level] << t1 << " " << t2 << std::endl;
+    return *this;
+  }
+  
+  template <typename T1, typename T2, typename T3>
+  log & log::operator() (const T1 & t1, const T2 & t2, const T3 & t3, log::verbosity level) {
+    if (globalverb < level) return *this;
+    for (int i = 0; i < indent; i++) logstream << "  ";
+    logstream << logstring[level] << t1 << " " << t2 << " " << t3 << std::endl;
+    return *this;
+  }
+  
+  template <typename T1, typename T2, typename T3, typename T4>
+  log & log::operator() (const T1 & t1, const T2 & t2, const T3 & t3, const T4 & t4, log::verbosity level) {
+    if (globalverb < level) return *this;
+    for (int i = 0; i < indent; i++) logstream << "  ";
+    logstream << logstring[level] << t1 << " " << t2 << " " << t3 << " " << t4 << std::endl;
+    return *this;
+  }
+  
+  template <typename T1, typename T2, typename T3, typename T4, typename T5>
+  log & log::operator() (const T1 & t1, const T2 & t2, const T3 & t3, const T4 & t4, const T5 & t5, log::verbosity level) {
+    if (globalverb < level) return *this;
+    for (int i = 0; i < indent; i++) logstream << "  ";
+    logstream << logstring[level] << t1 << " " << t2 << " " << t3 <<  " " << t4 << " " << t5 << std::endl;
+    return *this;
+  }
+  
+  template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
+  log & log::operator() (const T1 & t1, const T2 & t2, const T3 & t3, const T4 & t4, const T5 & t5, const T6 & t6, log::verbosity level) {
+    if (globalverb < level) return *this;
+    for (int i = 0; i < indent; i++) logstream << "  ";
+    logstream << logstring[level] << t1 << " " << t2 << " " << t3 <<  " " << t4 << " " << t5 << " " << t6 << std::endl;
+    return *this;
+  }
 }
 
-#define tracelog \
-  gear2d::log trace(__PRETTY_FUNCTION__, log::info);
+#ifdef _WIN32
+
+#define logtrace(level) \
+gear2d::log trace(__FUNCDNAME__, level)
+
+#define loginfo \
+gear2d::log trace(__FUNCDNAME__, gear2d::log::info)
+
+#define logverb \
+gear2d::log trace(__FUNCDNAME__, gear2d::log::verbose)
+
+#define logerr \
+gear2d::log trace(__FUNCDNAME__, gear2d::log::error)
+
+#define logwarn \
+gear2d::log trace(__FUNCDNAME__, gear2d::log::warning)
+
+#else // NOT _WIN32
+
+#define logtrace(level) \
+gear2d::log trace(__PRETTY_FUNCTION__, level)
+
+#define loginfo \
+gear2d::log trace(__PRETTY_FUNCTION__, gear2d::log::info)
+
+#define logverb \
+gear2d::log trace(__PRETTY_FUNCTION__, gear2d::log::verbose)
+
+#define logerr \
+gear2d::log trace(__PRETTY_FUNCTION__, gear2d::log::error)
+
+#define logwarn \
+gear2d::log trace(__PRETTY_FUNCTION__, gear2d::log::warning)
 #endif
+
+#endif // LOG_H
