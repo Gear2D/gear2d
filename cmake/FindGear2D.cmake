170927478e643f2d833cecd09ea98b2d7a416a80
Leonardo Guilherme de Freitas <leonardo.guilherme@gmail.com>
2013-02-10 16:54:21 -0200

    Updating FindGear2D

--- cmake/FindGear2D.cmake
+++ cmake/FindGear2D.cmake
@@ -11,25 +11,36 @@
 if (NOT Gear2D_FOUND)
 
 if (WIN32)
+	message(STATUS "Trying windoze $ENV{PROGRAMFILES}")
 	find_path(Gear2D_INCLUDE_DIR gear2d.h
 		HINTS
 		$ENV{GEAR2DDIR}
 		PATHS
-		"C:\\$ENV{PROGRAMFILES}\\Gear2D\\include"
-		"C:\\$ENV{PROGRAMFILES(X86)}\\Gear2D\\include"
+		"$ENV{PROGRAMFILES}\\Gear2D\\include"
+		"$ENV{PROGRAMFILES(X86)}\\Gear2D\\include"
 		"C:\\Gear2D\\include"
-		"C:\\$ENV{PROGRAMFILES}\\Gear2D\\include\\gear2d"
-		"C:\\$ENV{PROGRAMFILES(X86)}\\Gear2D\\include\\gear2d"
+		"$ENV{PROGRAMFILES}\\Gear2D\\include\\gear2d"
+		"$ENV{PROGRAMFILES(X86)}\\Gear2D\\include\\gear2d"
 		"C:\\Gear2D\\include\\gear2d"
 	)
 	
 	find_library(
 		Gear2D_LIBRARY gear2d
 		PATHS
-		"C:\\$ENV{PROGRAMFILES}\\Gear2D\\bin"
-		"C:\\$ENV{PROGRAMFILES(x86)}\\Gear2D\\bin"
+		"$ENV{PROGRAMFILES}\\Gear2D\\bin"
+		"$ENV{PROGRAMFILES(x86)}\\Gear2D\\bin"
 		"C:\\Gear2D\\bin"
 	)
+  
+  find_program(
+    Gear2D_EXECUTABLE gear2d.exe
+    HINTS
+    $ENV{GEAR2DDIR}
+    PATHS
+    "$ENV{PROGRAMFILES}\\Gear2D\\bin"
+		"$ENV{PROGRAMFILES(x86)}\\Gear2D\\bin"
+		"C:\\Gear2D\\bin"
+  )
 else ()
 	find_path(Gear2D_INCLUDE_DIR gear2d.h
 		HINTS
@@ -62,17 +73,35 @@ else ()
 		/usr/local
 		/usr
 	)
+  
+  find_program(
+    Gear2D_EXECUTABLE gear2d
+		HINTS
+		$ENV{GEAR2DDIR}
+		PATH_SUFFIXES bin
+		PATHS
+		/usr/local/
+		/usr/
+		/opt/local/
+		/opt
+  )
 endif(WIN32)
 
 
 
-IF(Gear2D_INCLUDE_DIR AND Gear2D_LIBRARY)
-	SET(Gear2D_FOUND TRUE)
-ENDIF(Gear2D_INCLUDE_DIR AND Gear2D_LIBRARY)
+IF(Gear2D_INCLUDE_DIR)
+	IF(Gear2D_LIBRARY)
+		SET(Gear2D_FOUND TRUE)
+	ELSE(Gear2D_LIBRARY)
+		message(ERROR "Missing Gear2D_LIBRARY")
+	ENDIF(Gear2D_LIBRARY)
+ELSE(Gear2D_INCLUDE_DIR)
+	message(ERROR "Missing Gear2D_INCLUDE_DIR")
+ENDIF(Gear2D_INCLUDE_DIR)
 
 IF(Gear2D_FOUND)
 	get_filename_component(Gear2D_LINK_DIR ${Gear2D_LIBRARY} PATH)
-    execute_process(COMMAND gear2d -v OUTPUT_VARIABLE Gear2D_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process(COMMAND ${Gear2D_EXECUTABLE} -v OUTPUT_VARIABLE Gear2D_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
 	SET(Gear2D_COMPONENT_PREFIX ${Gear2D_LINK_DIR}/../lib/gear2d/component)
 	IF (NOT Gear2D_FIND_QUIETLY)
 		MESSAGE(STATUS "Found Gear2D: ${Gear2D_LIBRARY} version ${Gear2D_VERSION}")
@@ -80,7 +109,7 @@ IF(Gear2D_FOUND)
 	ENDIF (NOT Gear2D_FIND_QUIETLY)
 ELSE(Gear2D_FOUND)
 	IF (Gear2D_FIND_REQUIRED)
-		MESSAGE(FATAL_ERROR "Could not find Gear2D")
+		MESSAGE(FATAL_ERROR "Could not find Gear2D.")
 	ENDIF (Gear2D_FIND_REQUIRED)
 ENDIF(Gear2D_FOUND)
 
