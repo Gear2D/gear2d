880a220f9220651c7232c02527bde117f0718f05
leo <lgfreitas@gear2d.com>
2013-11-04 23:43:21 -0200

    Updating FindGear2D.cmake to list gear2d-main.cc and to have gear2d_component macro

--- cmake/FindGear2D.cmake
+++ cmake/FindGear2D.cmake
@@ -8,6 +8,7 @@
 # Gear2D_LINK_DIR, where to find gear2d.so/gear2d.dll
 # Gear2D_COMPONENT_PREFIX, prefix to install components
 
+
 if (NOT Gear2D_FOUND)
 
 if (WIN32)
@@ -94,8 +95,6 @@ else ()
   )
 endif(WIN32)
 
-
-
 IF(Gear2D_INCLUDE_DIR)
   IF(Gear2D_LIBRARY)
     SET(Gear2D_FOUND TRUE)
@@ -108,9 +107,9 @@ ENDIF(Gear2D_INCLUDE_DIR)
 
 IF(Gear2D_FOUND)
   get_filename_component(Gear2D_LINK_DIR ${Gear2D_LIBRARY} PATH)
-    execute_process(COMMAND ${Gear2D_EXECUTABLE} -v OUTPUT_VARIABLE Gear2D_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
-  SET(Gear2D_COMPONENT_PREFIX ${Gear2D_LINK_DIR}/../lib/gear2d/component)
-  SET(Gear2D_MAIN ${Gear2D_LINK_DIR}/../share/gear2d/main.cc)
+  execute_process(COMMAND ${Gear2D_EXECUTABLE} -v OUTPUT_VARIABLE Gear2D_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+  get_filename_component(Gear2D_COMPONENT_PREFIX ${Gear2D_LINK_DIR}/../lib/gear2d/component ABSOLUTE)
+  get_filename_component(Gear2D_MAIN ${Gear2D_LINK_DIR}/../share/gear2d/main.cc ABSOLUTE)
   IF (NOT Gear2D_FIND_QUIETLY)
     MESSAGE(STATUS "Found Gear2D: ${Gear2D_LIBRARY} version ${Gear2D_VERSION}")
     MESSAGE(STATUS "Guessed Gear2D component prefix: ${Gear2D_COMPONENT_PREFIX}")
@@ -120,5 +119,17 @@ ELSE(Gear2D_FOUND)
     MESSAGE(FATAL_ERROR "Could not find Gear2D.")
   ENDIF (Gear2D_FIND_REQUIRED)
 ENDIF(Gear2D_FOUND)
+
+# Macro to help declare a component;
+# family is the family of the component, it defines where it will be installed;
+# type is the type of the component, it defines the library name;
+# sources keeps the source files for this component
+macro(gear2d_component family type sources)
+  message(STATUS "Component ${family}/${type} from sources ${sources}")
+  add_library(${type} MODULE ${sources})
+  add_library(${type}-static STATIC ${sources})
+  target_link_libraries(${type} ${Gear2D_LIBRARY})
+  install(TARGETS ${type} ${type}-static LIBRARY DESTINATION ${Gear2D_COMPONENT_PREFIX}/${family} ARCHIVE DESTINATION ${Gear2D_COMPONENT_PREFIX}/${family})
+endmacro()
 
 ENDIF(NOT Gear2D_FOUND)
