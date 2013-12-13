673419c5930b66b5b244669e7ba1c095dd8c04c3
leo <lgfreitas@gear2d.com>
2013-12-13 21:03:07 -0200

    Fixing indentation with tabs, spaces now.

--- Android.mk
+++ Android.mk
@@ -7,18 +7,18 @@ LOCAL_MODULE := gear2d_static
 LOCAL_CPPFLAGS := -std=c++11 -fexceptions -DLOGTRACE
 LOCAL_CFLAGS := -DYAML_VERSION_MAJOR=0 -DYAML_VERSION_MINOR=1 -DYAML_VERSION_PATCH=4 -DYAML_VERSION_STRING=\"0.1.4-with-SDLRwops\"
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/thirdparty/yaml-0.1.4/include \
-	$(LOCAL_PATH)/thirdparty/SDL2_framerate \
-	$(LOCAL_PATH)/$(SDL_PATH)/include
+  $(LOCAL_PATH)/thirdparty/SDL2_framerate \
+  $(LOCAL_PATH)/$(SDL_PATH)/include
   
 
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/src
 
 LOCAL_SRC_FILES := \
-	$(subst $(LOCAL_PATH)/,, \
-	$(wildcard $(LOCAL_PATH)/src/*.cc) \
-	$(wildcard $(LOCAL_PATH)/thirdparty/SDL2_framerate/*.c) \
-	$(wildcard $(LOCAL_PATH)/thirdparty/yaml-0.1.4/src/*.c))
+  $(subst $(LOCAL_PATH)/,, \
+  $(wildcard $(LOCAL_PATH)/src/*.cc) \
+  $(wildcard $(LOCAL_PATH)/thirdparty/SDL2_framerate/*.c) \
+  $(wildcard $(LOCAL_PATH)/thirdparty/yaml-0.1.4/src/*.c))
 
 LOCAL_MODULE_FILENAME := libgear2d
 
